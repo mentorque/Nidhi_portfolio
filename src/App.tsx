2a26cad397b715a8f08f5e2f0697b1ece4c2cb71
Yadav036 <86244076+Yadav036@users.noreply.github.com>
2025-12-18 18:23:30 +0530

    removed Professional summary

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
+//comments
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
